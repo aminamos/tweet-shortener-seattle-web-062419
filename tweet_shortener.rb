abd024bf5abe42059ab5b982381fe88309c70f7f
Amin Amos <aminamosahs@gmail.com>
2019-06-21 18:52:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,14 +27,17 @@ def word_substituter(tweet)
   # split the string into an array
   # iterate over the array
   tweet.split.each do |x|
-    # if the value in the current index matches any dictionary
-    # key
+    
+    # if the value in the current index
+    # matches any dictionary key
+    binding.pry
       if x.include?(dictionary.keys) == true
         # replace that index with the value
         tweet[x] = dictionary[value]
       end
   end
 end
+binding.pry
 
 def bulk_tweet_shortener(tweets)
   
