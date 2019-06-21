045daf5319e6d343941027cd893a7a7e649404b9
Amin Amos <aminamosahs@gmail.com>
2019-06-21 19:31:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -6,16 +6,16 @@ def dictionary
   # subs = Hash[first.zip(second)]
   
   {
-    "hello": "hi",
-    "to": "2",
-    "two": "2",
-    "too": "2",
-    "for": "4",
-    "four": "4",
-    "be": "b",
-    "you": "u",
-    "at": "@",
-    "and": "&"
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 
 end
