c7c637b74d85baf55d19e3dcc1852d83adde687b
Amin Amos <aminamosahs@gmail.com>
2019-06-21 20:04:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -57,7 +57,4 @@ def bulk_tweet_shortener(tweets)
   
   
 end
-"I'm running out of example tweets 4 u guys, which is weird, because I'm a writer & this is just writing & I tweet all day. 4 real, u guys.4 real."
-
-"I'm running out of example tweets 4 u guys, which is weird, because I'm a writer & this is just writing & I tweet all day. For real, u guys. For real."
 
