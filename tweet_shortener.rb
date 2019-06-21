4fe01b1d460b6c6b0b28de163ce4f013079053cb
Amin Amos <aminamosahs@gmail.com>
2019-06-21 19:34:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,7 +27,6 @@ def word_substituter(tweet)
   # split the string into an array
   # iterate over the array
   tweet.split.collect do |x|
-binding.pry
       if dictionary.keys.include?(x) == true
         # replace that index with the value
         binding.pry
